f572623bff8ee211b9d8d5d10bd2240e71ee0438
Michael Paul <michaelpaulwem@gmail.com>
2026-02-04 15:41:15 +0300

    some comments added

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import subprocess
 import datetime
 
+
 valid_script_commands = [
-    'py main.py',
-    'py main.py -v',
-    'py main.py -w',
-    'py main.py -x 100',
-    'py main.py -y 100',
-    'py main.py -x 100 -y 100',
-    'py main.py -y 200 -x 200'
+    'py main.py',                   # default script, i.e. processing wizards only
+    'py main.py -v',                # processing views
+    'py main.py -w',                # processing wizards
+    'py main.py -x 100',            # processing views with the width value of 100
+    'py main.py -y 100',            # processing views with the height value of 100
+    'py main.py -x 100 -y 100',     # processing views with the width and height values of 100
+    'py main.py -y 200 -x 200'      # processing views with the height and width values of 100
 ] 
 
 not_valid_script_commands = [
@@ -22,18 +23,16 @@ not_valid_script_commands = [
 
 script_number = 0
 print('starting the test...')
-print()
-for command in valid_script_commands:
+print() 
+for script in valid_script_commands:
     process = subprocess.Popen(
-        command,
-        stdin=subprocess.PIPE, 
-        stdout=subprocess.PIPE, 
-        stderr=subprocess.PIPE,
-        text=True
-    )
-    stdout, stderr = process.communicate(input='\n')
-    
-    print(f'"{valid_script_commands[script_number]}" finished at {datetime.datetime.now().strftime("%H:%M:%S")}')
-    
+        script, 
+        stdin   = subprocess.PIPE, 
+        stdout  = subprocess.PIPE, 
+        stderr  = subprocess.PIPE, 
+        text    = True
+        )
+    stdout, stderr = process.communicate(input='\n')          
+    print(f'"{valid_script_commands[script_number]}" finished at {datetime.datetime.now().strftime("%H:%M:%S")}')            
     script_number +=1
 print('the test is finshed')
