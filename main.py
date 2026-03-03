b6379d853cd2b615f1a624a58f1a9fbd0bcbe21a
Michael Paul <michaelpaulwem@gmail.com>
2026-03-03 16:16:53 +0300

    --dir flag added

--- main.py
+++ main.py
@@ -3,28 +3,77 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--wizard', action='store_true', dest='wizard', help='wizard flag')
-    parser.add_argument('-v', '--view',   action='store_true', dest='view',   help='view flag')
-    parser.add_argument('-t', '--type',   action='store_true', dest='type',   help='type flag')
-    parser.add_argument('-x', '--width',  action='store',      dest='width',  help='width value',  type=int, default=1271)
-    parser.add_argument('-y', '--height', action='store',      dest='height', help='height value', type=int, default=761)
-    parser.add_argument('-f', '--file',   action='store',      dest='path',   help='path value',   type=str, default=None)    
+    parser.add_argument('-w', '--wizard', action='store_true', dest='wizard',    help='wizard flag')
+    parser.add_argument('-v', '--view',   action='store_true', dest='view',      help='view flag')
+    parser.add_argument('-t', '--type',   action='store_true', dest='type',      help='type flag')
+    parser.add_argument('-d', '--dir',    action='store_true', dest='directory', help='directory flag')
+    parser.add_argument('-x', '--width',  action='store',      dest='width',     help='width value',  type=int, default=1271)
+    parser.add_argument('-y', '--height', action='store',      dest='height',    help='height value', type=int, default=761)
+    parser.add_argument('-f', '--file',   action='store',      dest='path',      help='path value',   type=str, default=None)    
     args = parser.parse_args()
 
     import script
     import misc
     # print(args)
-    match (args.wizard, args.view, args.path is not None, args.type):
-        case(True,  False, False, False): script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=None)
-        case(False, True,  False, False): script.main(wizard=False, view_width=args.width, view_height=args.height,  file_path=None)
-        case(True,  False, True, False):  script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=args.path)
-        case(False, True,  True, False):  script.main(wizard=False, view_width=args.width, view_height=args.height,  file_path=args.path)
-        case(False, False, _, False):     script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=None)
-        case(True,  True,  _, False):     misc.close_script_by_duplicated_flags()
-        case(_, _, _, True):              
+    match (args.wizard, args.view, args.path is not None, args.type, args.directory):
+        case(True,  False, False, False, False): script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=None)
+        case(False, True,  False, False, False): script.main(wizard=False, view_width=args.width, view_height=args.height,  file_path=None)
+        case(True,  False, True, False, False):  script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=args.path)
+        case(False, True,  True, False, False):  script.main(wizard=False, view_width=args.width, view_height=args.height,  file_path=args.path)
+        case(False, False, _, False, False):     script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=None)
+        case(True,  True,  _, False, False):     misc.close_script_by_duplicated_flags()
+        case(_, _, _, True, False):              
             directory, files = misc.get_input()
             script.print_target_pixels(directory, files)
-
+        case(False, False, _, False, True):
+            import os
+            directory = os.getcwd()            
+            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
+            targets = script.find_target_pixels(directory, files, wizard=True)
+            non_empty_files = script.get_new_list_of_files(targets)
+            edited_coordinates = script.edit_coordinates(targets, wizard=True)
+            script.crop_corners(
+                directory, 
+                non_empty_files, 
+                edited_coordinates, 
+                wizard=True, 
+                view_width=args.width, 
+                view_height=args.height
+                )
+            print(f'{misc.print_time()}', 'The script is finished.')
+        case(True, False, _, False, True):
+            import os
+            directory = os.getcwd()            
+            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
+            targets = script.find_target_pixels(directory, files, wizard=True)
+            non_empty_files = script.get_new_list_of_files(targets)
+            edited_coordinates = script.edit_coordinates(targets, wizard=True)
+            script.crop_corners(
+                directory, 
+                non_empty_files, 
+                edited_coordinates, 
+                wizard=True, 
+                view_width=args.width, 
+                view_height=args.height
+                )
+            print(f'{misc.print_time()}', 'The script is finished.')
+        case(False, True, _, False, True):
+            import os
+            directory = os.getcwd()            
+            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
+            targets = script.find_target_pixels(directory, files, wizard=False)
+            non_empty_files = script.get_new_list_of_files(targets)
+            edited_coordinates = script.edit_coordinates(targets, wizard=False)
+            script.crop_corners(
+                directory, 
+                non_empty_files, 
+                edited_coordinates, 
+                wizard=False, 
+                view_width=args.width, 
+                view_height=args.height
+                )
+            print(f'{misc.print_time()}', 'The script is finished.')
+        
 
 # TODO: add docstrings and type hints
 # TODO: add -s (or --size) flag to show the size of all screens STARTING from the target pixel
