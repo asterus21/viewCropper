18329ff52642f803c9521feed4effe77f217a17f
Michael Paul <michaelpaulwem@gmail.com>
2026-03-04 12:53:11 +0300

    some function refactorring

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import argparse
 
 
+directory, files = misc.get_input()
+
+def start_script(wizard=True):
+    import os
+    wizard = True
+    directory = os.getcwd()            
+    files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
+    targets = script.find_target_pixels(directory, files, wizard)
+    non_empty_files = script.get_new_list_of_files(targets)
+    edited_coordinates = script.edit_coordinates(targets, wizard)
+    script.crop_corners(
+        directory, 
+        non_empty_files, 
+        edited_coordinates, 
+        wizard, 
+        view_width=args.width, 
+        view_height=args.height
+        )
+    print(f'{misc.print_time()}', 'The script is finished.')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--wizard', action='store_true', dest='wizard',    help='wizard flag')
@@ -22,63 +43,15 @@ if __name__ == '__main__':
         case(False, True,  True, False, False):  script.main(wizard=False, view_width=args.width, view_height=args.height,  file_path=args.path)
         case(False, False, _, False, False):     script.main(wizard=True,  view_width=args.width, view_height=args.height,  file_path=None)
         case(True,  True,  _, False, False):     misc.close_script_by_duplicated_flags()
-        case(_, _, _, True, False):              
-            directory, files = misc.get_input()
-            script.print_target_pixels(directory, files)
-        case(False, False, _, False, True):
-            import os
-            directory = os.getcwd()            
-            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
-            targets = script.find_target_pixels(directory, files, wizard=True)
-            non_empty_files = script.get_new_list_of_files(targets)
-            edited_coordinates = script.edit_coordinates(targets, wizard=True)
-            script.crop_corners(
-                directory, 
-                non_empty_files, 
-                edited_coordinates, 
-                wizard=True, 
-                view_width=args.width, 
-                view_height=args.height
-                )
-            print(f'{misc.print_time()}', 'The script is finished.')
-        case(True, False, _, False, True):
-            import os
-            directory = os.getcwd()            
-            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
-            targets = script.find_target_pixels(directory, files, wizard=True)
-            non_empty_files = script.get_new_list_of_files(targets)
-            edited_coordinates = script.edit_coordinates(targets, wizard=True)
-            script.crop_corners(
-                directory, 
-                non_empty_files, 
-                edited_coordinates, 
-                wizard=True, 
-                view_width=args.width, 
-                view_height=args.height
-                )
-            print(f'{misc.print_time()}', 'The script is finished.')
-        case(False, True, _, False, True):
-            import os
-            directory = os.getcwd()            
-            files = [file for file in os.listdir(directory) if file.lower().endswith('.png') and not file.startswith('Cropped_')]
-            targets = script.find_target_pixels(directory, files, wizard=False)
-            non_empty_files = script.get_new_list_of_files(targets)
-            edited_coordinates = script.edit_coordinates(targets, wizard=False)
-            script.crop_corners(
-                directory, 
-                non_empty_files, 
-                edited_coordinates, 
-                wizard=False, 
-                view_width=args.width, 
-                view_height=args.height
-                )
-            print(f'{misc.print_time()}', 'The script is finished.')
+        case(_, _, _, True, False):              script.print_target_pixels(directory, files)
+        case(False, False, _, False, True):      start_script(wizard=True)
+        case(True, False, _, False, True):       start_script(wizard=True)
+        case(False, True, _, False, True):       start_script(wizard=False)
         
 
 # TODO: add docstrings and type hints
-# TODO: add -s (or --size) flag to show the size of all screens STARTING from the target pixel
+# TODO: add -s (or --size) flag to show the size of all screens
 # TODO: add -a (or --all) flag for the use of -s and -t together
-# TODO: add -d (or --dir) flag for not to specify the directory path
 # TODO: add -b (or --both) flag to process both views and wizards at the same time
 # TODO: add -c (or --cropped) flag to process ONLY those screenshots which start with "Cropped_", e.g. to edit already cropped screenshots
 # TODO: expand the description for the arguments
